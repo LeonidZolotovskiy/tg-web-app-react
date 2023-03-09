d16b9e09cca59f6e19b48a068a816664a9bed234
LeonidZolotovskiy <leonid_zolotovkiy@bk.ru>
2023-03-09 13:22:19 +0300

    Button3

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -13,7 +13,6 @@ export default function Form() {
       text: "Отправить заявку",
     });
   }, [tg.MainButton]);
-console.log();
   useEffect(() => {
     if (!street || !country) {
       tg.MainButton.hide();
